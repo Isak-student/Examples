3c8cc70d10a9ac7611fadc04f2b902928edf7a0c
Isak <zakusi@bk.ru>
2023-01-24 12:06:01 +0530

    11 пример

---
+++ Example0011/Program.cs
@@ -0,0 +1,26 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length; //  длинна массива//
+    int index = 0; // позиция индекса (в данном случае 0)//
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1,10); //new Random().Next(1,10) случайное число от 1 до 10//
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10]; //new int[10] - создай 10 элементов//
+
+FillArray(array);
+PrintArray(array);
